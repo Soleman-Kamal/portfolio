d1d359fc056beb3ce3c47ea2fddea4a8094826cc
Soleman-Kamal <raafkar935@gmail.com>
2026-07-01 14:33:53 +0300

    Update portfolio add whatsapp

--- src/Nav/Navbar.jsx
+++ src/Nav/Navbar.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+
 import "./Navbar.css";
 
 export default function Navbar() {
@@ -6,6 +8,15 @@ export default function Navbar() {
 
   const closeMenu = () => setShowMenu(false);
 
+  const handleWhatsApp = () => {
+    const phone = "972592589443";
+    const message = "مرحبًا، أريد الاستفسار عن خدماتك";
+
+    window.open(
+      `https://wa.me/${phone}?text=${encodeURIComponent(message)}`,
+      "_blank",
+    );
+  };
   return (
     <nav className="navbar">
       <div className="nav-glow"></div>
@@ -18,13 +29,32 @@ export default function Navbar() {
         </a>
 
         <ul className={showMenu ? "nav-links active" : "nav-links"}>
-          <li><a onClick={closeMenu} href="#home">Home</a></li>
-          <li><a onClick={closeMenu} href="#about">About</a></li>
-          <li><a onClick={closeMenu} href="#projects">Projects</a></li>
-          <li><a onClick={closeMenu} href="#contact">Contact</a></li>
+          <li>
+            <a onClick={closeMenu} href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#projects">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a onClick={closeMenu} href="#contact">
+              Contact
+            </a>
+          </li>
         </ul>
 
-        <a href="#contact" className="nav-cta">Hire Me</a>
+        <a href="#contact" className="nav-cta" onClick={handleWhatsApp} style={{display:"flex" , justifyContent:"center" , alignItems:"center"}}>
+          Hire Me
+          <WhatsAppIcon style={{fontSize:"20px"}}/>
+        </a>
 
         <button
           className={showMenu ? "menu-toggle active" : "menu-toggle"}
